c878badb7941170d6623e0c3f1ae38d4cad466af
Shubham Ambastha <shubham@f22labs.com>
2025-07-14 15:12:17 +0530

    feat: add DatabaseModule and IntegrationModule to AppModule for improved application structure

--- nest_module/src/app.module.ts
+++ nest_module/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { IntegrationModule } from './integration/integration.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule, IntegrationModule],
   controllers: [AppController],
   providers: [AppService],
 })
